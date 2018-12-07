8b3285c8dfdecde624b84a1c56945c15a9982724
rykos <rykos9999@gmail.com>
2018-12-07 18:43:06 +0100

    Initial commit

---
+++ Assets/Scripts/BuildingManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class BuildingManager : MonoBehaviour
+{
+    [SerializeField]
+    private string name;
+    [SerializeField]
+    private string description;
+    private Building x = new TestBuilding();
+
+    //Building collider clicked
+    private void OnMouseDown()
+    {
+        x.BuildingInteract();
+    }
+}
+
+public abstract class Building
+{
+    public string Name;
+    public string Description;
+    public int level;
+    public abstract void BuildingInteract();
+}
+
+public class TestBuilding : Building
+{
+    public override void BuildingInteract()
+    {
+        Debug.Log("Interacted");
+    }
+}
---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    private Vector2 clickStarted;
+    
+    private void Update()
+    {
+        Swipe();
+    }
+
+    private void Swipe()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            clickStarted = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+        if (Input.GetMouseButtonUp(0) && clickStarted != Vector2.zero)
+        {
+            Vector2 direction = (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition) - clickStarted;
+            clickStarted = Vector2.zero;
+            float degrees = SignedDegToDeg(-Vector2.SignedAngle(direction, Vector2.right));
+            DegToDirection(degrees);
+        }
+    }
+
+    private float SignedDegToDeg(float signedDeg)
+    {
+        if (signedDeg < 0)
+        {
+            signedDeg = 180 - (180 + signedDeg);
+        }
+        return signedDeg;
+    }
+
+    private Vector2 DegToDirection(float deg)
+    {
+        Vector2 direction;
+        if (deg <= 90 || deg > 270)// right
+        {
+            direction = Vector2.right;
+        }
+        else if (deg > 90 || deg <= 270)
+        {
+            direction = Vector2.left;
+        }
+        return Vector2.zero;
+    }
+}
